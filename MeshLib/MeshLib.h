939a40696374711c35c0f8da5cb1ce13d360750b
Mark Dawson <markdawson@live.com>
2017-12-27 09:26:21 -0800

    Initial Commit

---
+++ MeshLib/MeshLib.h
@@ -0,0 +1,19 @@
+//
+//  MeshLib.h
+//  MeshLib
+//
+//  Created by md on 12/27/17.
+//  Copyright © 2017 ruanestudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MeshLib.
+FOUNDATION_EXPORT double MeshLibVersionNumber;
+
+//! Project version string for MeshLib.
+FOUNDATION_EXPORT const unsigned char MeshLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MeshLib/PublicHeader.h>
+
+
